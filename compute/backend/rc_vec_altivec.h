5846a5929e6d7c4ce1d57bc34392b5940fc692c6
Hans-Peter Nilsson <hp@axis.com>
2011-03-03 07:10:49 +0100

    Fix bug #32618, AltiVec targets not building with recent (FSF) GCC.
    	* configure.ac (Pretentious names etc support) <altivec>: Check
    	supported syntax for vector literals, arrange to #define
    	RC_ALTIVEC_CURLY_BRACES.
    	* config.h.in, configure: Regenerate.
    	* compute/backend/rc_vec_altivec.h: (RC_ALTIVEC_INIT)
    	(RC_ALTIVEC_INIT16, RC_ALTIVEC_TINIT, RC_ALTIVEC_TINIT4)
    	(RC_ALTIVEC_TINIT8): New macros, defined differently conditional
    	on RC_ALTIVEC_CURLY_BRACES.  Use everywhere vector initializers
    	are used.
    	(RC_VEC_AVGZ, RC_VEC_ABS, RC_VEC_CMPGT, RC_VEC_CMPGE)
    	(RC_VEC_LERP, RC_VEC_LERPZ, RC_VEC_LERPN, RC_VEC_GETMASKV)
    	(RC_VEC_CNTV, RC_VEC_SUMV, RC_VEC_MACV, RC_VEC_SPLAT__)
    	(RC_VEC_LERP__): Add casts between vector types	as necessary for
    	type-correctness.
    	(RC_VEC_LOADU): Ditto.  Call vec_perm in the official API, not
    	vec_vperm.
    	(RC_VEC_BLEND, RC_VEC_BLENDZ): Use better-fitting int16_t, not uint16_t.

--- compute/backend/rc_vec_altivec.h
+++ compute/backend/rc_vec_altivec.h
@@ -40,6 +40,28 @@
 
 #include <altivec.h> /* AltiVec intrinsics */
 
+/**
+ *  See configure.ac. We stick to standard ISO syntax, deliberately
+ *  avoiding GCC extensions.
+ */
+#if RC_ALTIVEC_CURLY_BRACES
+/**
+ *  By specifying the minimally used number of arguments - 1, we
+ *  catch typos like forgetting the "16" suffix.
+ */ 
+#define RC_ALTIVEC_INIT(x1, x2, x3, ...) (rc_vec_t){x1, x2, x3, __VA_ARGS__}
+#define RC_ALTIVEC_INIT16(x) \
+    (rc_vec_t){x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x}
+#define RC_ALTIVEC_TINIT(type, x, ...) (type){x, __VA_ARGS__}
+#define RC_ALTIVEC_TINIT4(type, x) (type){x, x, x, x}
+#define RC_ALTIVEC_TINIT8(type, x) (type){x, x, x, x, x, x, x, x}
+#else
+#define RC_ALTIVEC_INIT(x1, x2, x3, ...) (rc_vec_t)(x1, x2, x3, __VA_ARGS__)
+#define RC_ALTIVEC_INIT16(x) (rc_vec_t)(x)
+#define RC_ALTIVEC_TINIT(type, x, ...) (type)(x, __VA_ARGS__)
+#define RC_ALTIVEC_TINIT4(type, x) (type)(x)
+#define RC_ALTIVEC_TINIT8(type, x) (type)(x)
+#endif
 
 /*
  * -------------------------------------------------------------
@@ -136,7 +158,7 @@ do {                                                                        \
 #define RC_VEC_LOADU(dstv, vec1, vec2, vec3, uptr)                       \
 do {                                                                     \
     rc_vec_t vec__ = vec_ld(0, (const uint8_t*)(uptr)); /* Load next  */ \
-    (dstv) = vec_vperm(vec2, vec__, vec1);              /* Permute    */ \
+    (dstv) = vec_perm(vec2, vec__, vec1);               /* Permute    */ \
     (vec2) = vec__;                                     /* Update old */ \
 } while (0)
 
@@ -169,13 +191,13 @@ do {                                                                     \
  *  Shift all fields to the logical left by a constant value.
  */
 #define RC_VEC_SHLC(dstv, srcv, bytes) \
-    ((dstv) = vec_slo(srcv, (rc_vec_t)(8*(bytes))))
+    ((dstv) = vec_slo(srcv, RC_ALTIVEC_INIT16(8*(bytes))))
 
 /**
  *  Shift fields to the logical right by a constant value.
  */
 #define RC_VEC_SHRC(dstv, srcv, bytes) \
-    ((dstv) = vec_sro(srcv, (rc_vec_t)(8*(bytes))))
+    ((dstv) = vec_sro(srcv, RC_ALTIVEC_INIT16(8*(bytes))))
 
 /**
  *  Align srcv1 and srcv2 to dstv, starting at field @e bytes
@@ -190,10 +212,10 @@ do {                                                                     \
  *  with fields 0, ..., RC_VEC_SIZE/2 - 1 from srcv1 and
  *  fields RC_VEC_SIZE/2, ..., RC_VEC_SIZE - 1 from srcv2.
  */
-#define RC_VEC_PACK(dstv, srcv1, srcv2)                            \
-    ((dstv) = vec_perm(srcv1, srcv2,                               \
-                       (rc_vec_t)( 0,  2,  4,  6,  8, 10, 12, 14,  \
-                                  16, 18, 20, 22, 24, 26, 28, 30)))
+#define RC_VEC_PACK(dstv, srcv1, srcv2)                                 \
+    ((dstv) = vec_perm(srcv1, srcv2,                                    \
+                       RC_ALTIVEC_INIT( 0,  2,  4,  6,  8, 10, 12, 14,  \
+                                       16, 18, 20, 22, 24, 26, 28, 30)))
 
 /*
  * -------------------------------------------------------------
@@ -205,7 +227,7 @@ do {                                                                     \
  *  Set all bits to zero.
  */
 #define RC_VEC_ZERO(vec) \
-    ((vec) = (rc_vec_t)(0))
+    ((vec) = RC_ALTIVEC_INIT16(0))
 
 /**
  *  Bitwise NOT.
@@ -289,7 +311,8 @@ do {                                           \
     rc_vec_t adj__;                            \
     adj__  = vec_xor(sv1__, sv2__);            \
     sv1__  = vec_avg(sv1__, sv2__);            \
-    adj__  = vec_and(adj__, (rc_vec_t)(0x01)); \
+    adj__  = vec_and(adj__,                    \
+                     RC_ALTIVEC_INIT16(0x01)); \
     (dstv) = vec_sub(sv1__, adj__);            \
 } while (0)
 
@@ -311,9 +334,11 @@ do {                                           \
     rc_vec_t sv2__ = (srcv2);                  \
     rc_vec_t avg__ = vec_avg(sv1__, sv2__);    \
     rc_vec_t xor__ = vec_xor(sv1__, sv2__);    \
-    rc_vec_t cmp__ = vec_cmpgt(sv2__, sv1__);  \
+    rc_vec_t cmp__ = (rc_vec_t)                \
+                     vec_cmpgt(sv2__, sv1__);  \
     xor__  = vec_and(xor__, cmp__);            \
-    xor__  = vec_and(xor__, (rc_vec_t)(0x01)); \
+    xor__  = vec_and(xor__,                    \
+                     RC_ALTIVEC_INIT16(0x01)); \
     (dstv) = vec_sub(avg__, xor__);            \
 } while (0)
 
@@ -357,8 +382,10 @@ do {                                                                  \
 #define RC_VEC_ABS(dstv, srcv)                \
 do {                                          \
     rc_vec_t vec__;                           \
-    vec__  = vec_xor(srcv, (rc_vec_t)(0x80)); \
-    vec__  = vec_abs((vector int8_t)vec__);   \
+    vec__  = vec_xor(srcv,                    \
+                     RC_ALTIVEC_INIT16(0x80));\
+    vec__  = (rc_vec_t)vec_abs((vector int8_t)\
+                               vec__);        \
     (dstv) = vec_adds(vec__, vec__);          \
 } while (0)
 
@@ -367,7 +394,7 @@ do {                                          \
  *  Computes dstv = srcv1 > srcv2 ? 0x80 : 0 for each 8-bit field.
  */
 #define RC_VEC_CMPGT(dstv, srcv1, srcv2) \
-    ((dstv) = vec_cmpgt(srcv1, srcv2))
+    ((dstv) = (rc_vec_t)vec_cmpgt(srcv1, srcv2))
 
 /**
  *  Comparison.
@@ -375,7 +402,8 @@ do {                                          \
  */
 #define RC_VEC_CMPGE(dstv, srcv1, srcv2)     \
 do {                                         \
-    rc_vec_t gt__ = vec_cmpgt(srcv2, srcv1); \
+    rc_vec_t gt__ = (rc_vec_t)               \
+                    vec_cmpgt(srcv2, srcv1); \
     (dstv) = vec_nor(gt__, gt__);            \
 } while (0)
 
@@ -397,7 +425,7 @@ do {                                         \
  *  Generate the blend vector needed by RC_VEC_LERP().
  */
 #define RC_VEC_BLEND(blendv, blend8) \
-    RC_VEC_SPLAT__(blendv, (blend8) << 7, uint16_t)
+    RC_VEC_SPLAT__(blendv, (blend8) << 7, int16_t)
 
 /**
  *  Linear interpolation.
@@ -408,16 +436,24 @@ do {                                         \
 do {                                                         \
     rc_vec_t       sv1__ = (srcv1);                          \
     rc_vec_t       sv2__ = (srcv2);                          \
-    vector int16_t bv__  = (blendv);                         \
-    vector int16_t lo1__ = vec_mergel((rc_vec_t)(0), sv1__); \
-    vector int16_t hi1__ = vec_mergeh((rc_vec_t)(0), sv1__); \
-    vector int16_t lo2__ = vec_mergel((rc_vec_t)(0), sv2__); \
-    vector int16_t hi2__ = vec_mergeh((rc_vec_t)(0), sv2__); \
+    vector int16_t bv__  = (vector int16_t)(blendv);         \
+    vector int16_t lo1__ = (vector int16_t)                  \
+                           vec_mergel(RC_ALTIVEC_INIT16(0),  \
+                                      sv1__);                \
+    vector int16_t hi1__ = (vector int16_t)                  \
+                           vec_mergeh(RC_ALTIVEC_INIT16(0),  \
+                                      sv1__);                \
+    vector int16_t lo2__ = (vector int16_t)                  \
+                           vec_mergel(RC_ALTIVEC_INIT16(0),  \
+                                      sv2__);                \
+    vector int16_t hi2__ = (vector int16_t)                  \
+                           vec_mergeh(RC_ALTIVEC_INIT16(0),  \
+                                      sv2__);                \
     lo2__  = vec_sub(lo2__, lo1__);                          \
     hi2__  = vec_sub(hi2__, hi1__);                          \
     lo1__  = vec_mradds(lo2__, bv__, lo1__);                 \
     hi1__  = vec_mradds(hi2__, bv__, hi1__);                 \
-    (dstv) = vec_pack(hi1__, lo1__);                         \
+    (dstv) = (rc_vec_t)vec_pack(hi1__, lo1__);               \
 } while (0)
 
 
@@ -425,7 +461,7 @@ do {                                                         \
  *  Generate the blend vector needed by RC_VEC_LERPZ().
  */
 #define RC_VEC_BLENDZ(blendv, blend8) \
-    RC_VEC_SPLAT__(blendv, blend8, uint16_t)
+    RC_VEC_SPLAT__(blendv, blend8, int16_t)
 
 /**
  *  Linear interpolation rounded towards srcv1.
@@ -437,7 +473,7 @@ do {                                                         \
 do {                                                      \
     rc_vec_t sv1__ = (srcv1);                             \
     rc_vec_t sv2__ = (srcv2);                             \
-    rc_vec_t bias__ = vec_cmpgt(sv1__, sv2__);            \
+    rc_vec_t bias__ = (rc_vec_t)vec_cmpgt(sv1__, sv2__);  \
     RC_VEC_LERP__(dstv, sv1__, sv2__, blend8v, bias__);   \
 } while (0)
 
@@ -445,7 +481,7 @@ do {                                                      \
  *  Generate the blend vector needed by RC_VEC_LERPN().
  */
 #define RC_VEC_BLENDN(blendv, blend8) \
-    RC_VEC_SPLAT__(blendv, blend8, uint16_t)
+    RC_VEC_SPLAT__(blendv, blend8, int16_t)
 
 /**
  *  Linear interpolation rounded towards srcv2.
@@ -457,7 +493,7 @@ do {                                                      \
 do {                                                      \
     rc_vec_t sv1__ = (srcv1);                             \
     rc_vec_t sv2__ = (srcv2);                             \
-    rc_vec_t bias__ = vec_cmpgt(sv2__, sv1__);            \
+    rc_vec_t bias__ = (rc_vec_t)vec_cmpgt(sv2__, sv1__);  \
     RC_VEC_LERP__(dstv, sv1__, sv2__, blend8v, bias__);   \
 } while (0)
 
@@ -476,14 +512,19 @@ do {                                                      \
 #define RC_VEC_GETMASKV(maskv, vec)                                     \
 do {                                                                    \
     rc_vec_t        vec__  = (vec);                                     \
-    vector unsigned mask__ = (vector unsigned)(0x80402010, 0x08040201,  \
-                                               0x80402010, 0x08040201); \
-    vec__   = vec_cmpgt(vec__, (rc_vec_t)(0x7f));                       \
-    mask__  = vec_and(vec__, (rc_vec_t)mask__);                         \
-    mask__  = vec_sum4s((rc_vec_t)mask__, (vector unsigned)(0));        \
-    mask__  = vec_sl(mask__, (vector unsigned)(8, 8, 0, 0));            \
-    mask__  = vec_sums((vector int)mask__, (vector int)(0));            \
-    (maskv) = vec_slo((rc_vec_t)mask__, (rc_vec_t)(8*14));              \
+    vector unsigned mask__ = RC_ALTIVEC_TINIT(vector unsigned,          \
+                                              0x80402010, 0x08040201,   \
+                                              0x80402010, 0x08040201);  \
+    vec__   = (rc_vec_t)vec_cmpgt(vec__, RC_ALTIVEC_INIT16(0x7f));      \
+    mask__  = vec_and((vector unsigned)vec__, mask__);                  \
+    mask__  = vec_sum4s((rc_vec_t)mask__,                               \
+                        RC_ALTIVEC_TINIT4(vector unsigned, 0));         \
+    mask__  = vec_sl(mask__,                                            \
+                     RC_ALTIVEC_TINIT(vector unsigned, 8, 8, 0, 0));    \
+    mask__  = (vector unsigned)                                         \
+              vec_sums((vector int)mask__,                              \
+                       RC_ALTIVEC_TINIT4(vector int, 0));               \
+    (maskv) = vec_slo((rc_vec_t)mask__, RC_ALTIVEC_INIT16(8*14));       \
 } while (0)
 
 
@@ -507,20 +548,23 @@ do {                                                                    \
  */
 #define RC_VEC_CNTV(accv, srcv)                                          \
 do {                                                                     \
-    vector int c1__  = (vector int)(0x55555555);                         \
-    vector int c2__  = (vector int)(0x33333333);                         \
-    vector int c4__  = (vector int)(0x0f0f0f0f);                         \
-    vector int acc__ = (srcv);                                           \
+    vector int c1__  = RC_ALTIVEC_TINIT4(vector int, 0x55555555);        \
+    vector int c2__  = RC_ALTIVEC_TINIT4(vector int, 0x33333333);        \
+    vector int c4__  = RC_ALTIVEC_TINIT4(vector int, 0x0f0f0f0f);        \
+    vector int acc__ = (vector int)(srcv);                               \
     vector int tmp__;                                                    \
-    tmp__  = vec_sr(acc__, (vector unsigned)(1)); /* tmp  = acc >> 1  */ \
+    tmp__  = vec_sr(acc__,                        /* tmp  = acc >> 1  */ \
+                    RC_ALTIVEC_TINIT4(vector unsigned, 1));              \
     tmp__  = vec_and(tmp__, c1__);                /* tmp &= 0x55...   */ \
     acc__  = vec_and(acc__, c1__);                /* acc &= 0x55...   */ \
     acc__  = vec_add(acc__, tmp__);               /* acc += tmp       */ \
-    tmp__  = vec_sr(acc__, (vector unsigned)(2)); /* tmp  = acc >> 2  */ \
+    tmp__  = vec_sr(acc__,                        /* tmp  = acc >> 2  */ \
+                    RC_ALTIVEC_TINIT4(vector unsigned, 2));              \
     tmp__  = vec_and(tmp__, c2__);                /* tmp &= 0x33...   */ \
     acc__  = vec_and(acc__, c2__);                /* acc &= 0x33...   */ \
     acc__  = vec_add(acc__, tmp__);               /* acc += tmp       */ \
-    tmp__  = vec_sr(acc__, (vector unsigned)(4)); /* tmp  = acc >> 4  */ \
+    tmp__  = vec_sr(acc__,                        /* tmp  = acc >> 4  */ \
+                    RC_ALTIVEC_TINIT4(vector unsigned, 4));              \
     acc__  = vec_add(acc__, tmp__);               /* acc += tmp       */ \
     acc__  = vec_and(acc__, c4__);                /* acc &= 0x0f...   */ \
     RC_VEC_SUMV(accv, (rc_vec_t)acc__);                                  \
@@ -545,13 +589,14 @@ do {                                                                     \
  *  Sum all 8-bit fields, accumulation step.
  */
 #define RC_VEC_SUMV(accv, srcv) \
-    ((accv) = vec_sum4s(srcv, (vector unsigned)(accv)))
+    ((accv) = (rc_vec_t)vec_sum4s(srcv, (vector unsigned)(accv)))
 
 /**
  *  Sum all 8-bit fields, reduction step.
  */
 #define RC_VEC_SUMR(sum, accv) \
-    RC_VEC_VTOI__(sum, vec_sums((vector int)(accv), (vector int)(0)), int)
+    RC_VEC_VTOI__(sum, vec_sums((vector int)(accv), \
+                                RC_ALTIVEC_TINIT4(vector int, 0)), int)
 
 /**
  *  Multiply and accumulate all 8-bit fields.
@@ -566,7 +611,7 @@ do {                                                                     \
  *  Multiply and accumulate all 8-bit fields, accumulation step.
  */
 #define RC_VEC_MACV(accv, srcv1, srcv2) \
-    ((accv) = vec_msum(srcv1, srcv2, (vector unsigned)(accv)))
+    ((accv) = (rc_vec_t)vec_msum(srcv1, srcv2, (vector unsigned)(accv)))
 
 /**
  *  Multiply and accumulate all 8-bit fields, reduction step.
@@ -589,8 +634,10 @@ do {                                           \
     type        scal__ = (scal);               \
     rc_vec_t    map__  = vec_lvsl(0, &scal__); \
     vector type vec__  = vec_lde(0, &scal__);  \
-    map__ = vec_splat((vector type)map__, 0);  \
-    (vec) = vec_perm(vec__, vec__, map__);     \
+    map__ = (rc_vec_t)                         \
+            vec_splat((vector type)map__, 0);  \
+    (vec) = (rc_vec_t)                         \
+            vec_perm(vec__, vec__, map__);     \
 } while (0)
 
 /**
@@ -610,20 +657,34 @@ do {                                                              \
  */
 #define RC_VEC_LERP__(dstv, srcv1, srcv2, blend8v, bias)     \
 do {                                                         \
-    vector int16_t bv__  = (blendv);                         \
-    vector int16_t lob__ = vec_mergel((rc_vec_t)(0), bias);  \
-    vector int16_t hib__ = vec_mergeh((rc_vec_t)(0), bias);  \
-    vector int16_t lo1__ = vec_mergel((rc_vec_t)(0), srcv1); \
-    vector int16_t hi1__ = vec_mergeh((rc_vec_t)(0), srcv1); \
-    vector int16_t lo2__ = vec_mergel((rc_vec_t)(0), srcv2); \
-    vector int16_t hi2__ = vec_mergeh((rc_vec_t)(0), srcv2); \
+    vector int16_t bv__  = (vector int16_t)(blendv);         \
+    vector int16_t lob__ = (vector int16_t)                  \
+                           vec_mergel(RC_ALTIVEC_INIT16(0),  \
+                                      bias);                 \
+    vector int16_t hib__ = (vector int16_t)                  \
+                           vec_mergeh(RC_ALTIVEC_INIT16(0),  \
+                                      bias);                 \
+    vector int16_t lo1__ = (vector int16_t)                  \
+                           vec_mergel(RC_ALTIVEC_INIT16(0),  \
+                                      srcv1);                \
+    vector int16_t hi1__ = (vector int16_t)                  \
+                           vec_mergeh(RC_ALTIVEC_INIT16(0),  \
+                                      srcv1);                \
+    vector int16_t lo2__ = (vector int16_t)                  \
+                           vec_mergel(RC_ALTIVEC_INIT16(0),  \
+                                      srcv2);                \
+    vector int16_t hi2__ = (vector int16_t)                  \
+                           vec_mergeh(RC_ALTIVEC_INIT16(0),  \
+                                      srcv2);                \
     lo2__  = vec_sub(lo2__, lo1__);                          \
     hi2__  = vec_sub(hi2__, hi1__);                          \
     lo1__  = vec_mladd(lo2__, bv__, lob__);                  \
     hi1__  = vec_mladd(hi2__, bv__, hib__);                  \
-    lo1__  = vec_sr(lo1__, (vector uint16_t)(8));            \
-    hi1__  = vec_sr(hi1__, (vector uint16_t)(8));            \
-    (dstv) = vec_pack(hi1__, lo1__);                         \
+    lo1__  = vec_sr(lo1__,                                   \
+                    RC_ALTIVEC_TINIT8(vector uint16_t, 8));  \
+    hi1__  = vec_sr(hi1__,                                   \
+                    RC_ALTIVEC_TINIT8(vector uint16_t, 8));  \
+    (dstv) = (rc_vec_t)vec_pack(hi1__, lo1__);               \
     (dstv) = vec_add(dstv, srcv1);                           \
 } while (0)
 
